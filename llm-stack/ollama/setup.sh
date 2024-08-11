3c97cfb8119039727973b4e22146f0f55769d259
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-08-11 20:52:58 +0900

    :sparkles: Fix Modelfile of watashiha

--- llm-stack/ollama/setup.sh
+++ llm-stack/ollama/setup.sh
@@ -1,9 +1,9 @@
 cd /root
 
 # Setup
-apt update
-apt upgrade -y
-apt autoremove -y
+# apt update
+# apt upgrade -y
+# apt autoremove -y
 apt install -y wget
 
 # Pull LLM models
@@ -16,6 +16,7 @@ ollama pull deepseek-coder-v2:16b
 # ollama pull deepseek-coder:6.7b
 # ollama pull deepseek-coder:33b
 ollama pull nomic-embed-text:latest
+# ollama pull llava:7b
 
 # Pull LLM models without library
 if [ ! -e ./ollama/elyza3/*.gguf ]; then
@@ -24,8 +25,14 @@ if [ ! -e ./ollama/elyza3/*.gguf ]; then
 fi
 ollama create elyza3:8b -f ./ollama/elyza3/Modelfile
 
-# if [ ! -e ./ollama/watashiha/*.gguf ]; then
-#     echo "Download watashiha:6b"
-#     wget -P ./ollama/watashiha https://huggingface.co/mmnga/watashiha-gpt-6b-gguf/resolve/main/watashiha-gpt-6b-q4_K_M.gguf
-# fi
-# ollama create watashiha:6b -f ./ollama/watashiha/Modelfile
+if [ ! -e ./ollama/llm-jp/*.gguf ]; then
+    echo "Download llm-jp:13b"
+    wget -P ./ollama/llm-jp https://huggingface.co/mmnga/llm-jp-13b-instruct-full-ac_001_16x-dolly-ichikara_004_001_single-oasst-oasst2-v2.0-gguf/resolve/main/llm-jp-13b-instruct-full-ac_001_16x-dolly-ichikara_004_001_single-oasst-oasst2-v2.0-Q4_K_M.gguf
+fi
+ollama create llm-jp:13b -f ./ollama/llm-jp/Modelfile
+
+if [ ! -e ./ollama/watashiha/*.gguf ]; then
+    echo "Download watashiha:6b"
+    wget -P ./ollama/watashiha https://huggingface.co/mmnga/watashiha-gpt-6b-gguf/resolve/main/watashiha-gpt-6b-q4_K_M.gguf
+fi
+ollama create watashiha:6b -f ./ollama/watashiha/Modelfile
