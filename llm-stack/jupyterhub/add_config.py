debcb6ada85eeab03b893638fcf6f4a8b4a9e306
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-09-30 19:46:25 +0900

    A

--- llm-stack/jupyterhub/add_config.py
+++ llm-stack/jupyterhub/add_config.py
@@ -1,43 +1,42 @@
 # from oauthenticator.gitlab import GitLabOAuthenticator
 from oauthenticator.gitlab import LocalGitLabOAuthenticator
+import os
 
+### PAM Authenticator
+# c.PAMAuthenticator.admin_groups = {"sudo"}
+# c.Authenticator.admin_users = {
+#     "vscode",
+# }
+# c.Authenticator.allowed_users = {
+#     "vscode",
+# }
 
+### GitLab OAuth
 c.JupyterHub.authenticator_class = LocalGitLabOAuthenticator
-c.LocalGitLabOAuthenticator.gitlab_host = "http://192.168.11.2/gitlab/"
-c.LocalGitLabOAuthenticator.client_id = (
-    "48bd9e11ee2117910cdc3c9aa56270ffbed41d59ddb6b980e651a589b32c316e"
-)
-
-c.LocalGitLabOAuthenticator.client_secret = (
-    "gloas-a243b83b36ba9e6ce9b2fcdcb8b0e5649d8e67eeb95b6b3ddd66c26c7d6e2131"
-)
-c.LocalGitLabOAuthenticator.oauth_callback_url = (
-    "http://localhost/jupyterhub/hub/oauth_callback"
-)
+c.LocalGitLabOAuthenticator.oauth_callback_url = str(os.getenv("OAUTH_CALLBACK_URL"))
+c.LocalGitLabOAuthenticator.client_id = str(os.getenv("GITLAB_CLIENT_ID"))
+c.LocalGitLabOAuthenticator.client_secret = str(os.getenv("GITLAB_CLIENT_SECRET"))
 c.Authenticator.allow_all = True
 
+### Domain settings
 c.JupyterHub.base_url = "/jupyterhub"
-# ログイン後に http://...:8000/user/<username>/lab? へ遷移する設定（Jupyterlabが起動）
 c.Spawner.default_url = "/lab"
-# Jupyterlabで作成されたノートブックファイルなどが格納されるディレクトリ
+
+### User settings
 c.Spawner.notebook_dir = "~/notebook"
-# vscodeユーザのユーザ名
-c.Authenticator.admin_users = {
-    "vscode",
-}
-c.Authenticator.allowed_users = {
-    "vscode",
-}
-# c.PAMAuthenticator.admin_groups = {"sudo"}
 c.LocalAuthenticator.create_system_users = True
 c.LocalAuthenticator.add_user_cmd = ["/etc/jupyterhub/add_user.sh"]
+
+### Timeout
+# Timeout after start notebook
 c.Spawner.start_timeout = 120
+# Timeout after connection
 c.Spawner.http_timeout = 60
-# 30日 非アクティブのJupyterサーバーのシャットダウン期限
+
+### Auto shutdown
+# Auto shutdown inactive notebook after 30 days
 c.NotebookApp.shutdown_no_activity_timeout = 30 * 24 * 60 * 60
-
-# 3日 カーネルのシャットダウン期限
+# Auto shutdown inactive kernel after 3 days
 c.MappingKernelManager.cull_idle_timeout = 3 * 24 * 60 * 60
-
-# 1時間 シャットダウンするかどうかの確認インターバル
+# Check if shutdown once 1 hour
 c.MappingKernelManager.cull_internal = 60 * 60
--- llm-stack/jupyterhub/add_user.sh
+++ llm-stack/jupyterhub/add_user.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+### User creation script
+
 USERNAME="${1}"
 PASSWORD="${USERNAME}"
 
-# ユーザー追加、パスワード固定
 useradd -s /bin/bash -m ${USERNAME} && echo -e "${PASSWORD}\n${PASSWORD}" | passwd ${USERNAME}
-# sudoグループに追加
 gpasswd -a ${USERNAME} sudo
 
 mkdir -p /home/${USERNAME}/notebook
