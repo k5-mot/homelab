3a94a64545ae808bc47f10a6fd0eeaba5e69471a
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-09-30 19:21:54 +0900

    Update

--- llm-stack/jupyterhub/add_config.py
+++ llm-stack/jupyterhub/add_config.py
@@ -1,18 +1,19 @@
-# from oauthenticator.gitlab import LocalGitLabOAuthenticator
 # from oauthenticator.gitlab import GitLabOAuthenticator
+from oauthenticator.gitlab import LocalGitLabOAuthenticator
 
-# c.JupyterHub.authenticator_class = LocalGitLabOAuthenticator
-# c.LocalGitLabOAuthenticator.gitlab_host = "http://localhost/gitlab/"
-# c.LocalGitLabOAuthenticator.client_id = (
-#     "ba17208cf245b581f86c9a00c4311d6094c9144094d41acf25cfcf5b3b767f3d"
-# )
 
-# c.LocalGitLabOAuthenticator.client_secret = (
-#     "gloas-752a4477483d303b2e4b6347bce47c8a4c59c1ac5e6bb2a41ea6ac1634fdc23c"
-# )
-# c.LocalGitLabOAuthenticator.oauth_callback_url = (
-#     "http://localhost/jupyterhub/hub/oauth_callback"
-# )
+c.JupyterHub.authenticator_class = LocalGitLabOAuthenticator
+c.LocalGitLabOAuthenticator.gitlab_host = "http://192.168.11.2/gitlab/"
+c.LocalGitLabOAuthenticator.client_id = (
+    "48bd9e11ee2117910cdc3c9aa56270ffbed41d59ddb6b980e651a589b32c316e"
+)
+
+c.LocalGitLabOAuthenticator.client_secret = (
+    "gloas-a243b83b36ba9e6ce9b2fcdcb8b0e5649d8e67eeb95b6b3ddd66c26c7d6e2131"
+)
+c.LocalGitLabOAuthenticator.oauth_callback_url = (
+    "http://localhost/jupyterhub/hub/oauth_callback"
+)
 c.Authenticator.allow_all = True
 
 c.JupyterHub.base_url = "/jupyterhub"
@@ -27,7 +28,7 @@ c.Authenticator.admin_users = {
 c.Authenticator.allowed_users = {
     "vscode",
 }
-c.PAMAuthenticator.admin_groups = {"sudo"}
+# c.PAMAuthenticator.admin_groups = {"sudo"}
 c.LocalAuthenticator.create_system_users = True
 c.LocalAuthenticator.add_user_cmd = ["/etc/jupyterhub/add_user.sh"]
 c.Spawner.start_timeout = 120
